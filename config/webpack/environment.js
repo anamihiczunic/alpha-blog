a660d87177dc52421dc83969c22813a12e301fbb
Ana Mihic Zunic <anamihiczunic@gmail.com>
2021-01-07 11:46:56 +0100

    Add module enviroment

---
+++ config/webpack/environment.js
@@ -0,0 +1,24 @@
+const {environment} = require ('@ rails/webpacker')
+const {VueLoaderPlugin} = require ('vue-loader')
+const vue = require ('./ loaders/vue')module.exports = {
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin ()
+  ]
+}
+module.exports = environment
+environment.plugins.prepend ('VueLoaderPlugin', new VueLoaderPlugin ())
+environment.loaders.prepend ('vue', vue)
+module.exports = environment
+const customConfig = require ('./ custom')
+environment.config.merge (customConfig)
+module.exports = environments
